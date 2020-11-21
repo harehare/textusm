e85082170c74b2b1cb25e8fc8e23e8a7c6a3484b
Takahiro Sato <harehare1110@gmail.com>
2020-11-21 23:31:41 +0900

    Remove unnecessary config

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -3,18 +3,7 @@ module.exports = {
         content: ["./src/index.html", "./src/**/*.elm", "./src/**/*.ts"],
     },
     theme: {
-        extend: {
-            colors: {
-                cyan: "#9cdbff",
-            },
-            margin: {
-                default: "8px",
-            },
-            padding: {
-                base: "8px",
-                lg: "16px",
-            },
-        },
+        extend: {},
     },
     variants: {},
     plugins: [],
