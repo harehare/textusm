55cd6401d94ce4af9a9d18ff543b45881370b46d
harehare <harehare1110@gmail.com>
2022-06-03 22:23:28 +0900

    :wrench: Update cypress config

---
+++ web/cypress.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+    e2e: {
+        baseUrl: 'http://localhost:3000',
+        specPattern: 'e2e/**/*.js',
+        supportFile: false,
+    },
+});
