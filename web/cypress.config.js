27c5f15d6d75c81ea705017727f7141dd8801393
harehare <harehare1110@gmail.com>
2022-09-21 22:01:36 +0900

    :white_check_mark: Add retries to cypress.config.js

--- web/cypress.config.js
+++ web/cypress.config.js
@@ -1,12 +1,11 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-    e2e: {
-        baseUrl: process.env.USE_HTTPS
-            ? 'https://localhost:3000'
-            : 'http://localhost:3000',
-        specPattern: 'e2e/**/*.js',
-        supportFile: false,
-        defaultCommandTimeout: 10000,
-    },
+  e2e: {
+    baseUrl: process.env.USE_HTTPS ? 'https://localhost:3000' : 'http://localhost:3000',
+    specPattern: 'e2e/**/*.js',
+    supportFile: false,
+    defaultCommandTimeout: 10_000,
+    retries: 3,
+  },
 });
