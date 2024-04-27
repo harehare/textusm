1c9abf6482c2ba0edef832e043aeb03c52df9475
harehare <harehare1110@gmail.com>
2024-04-27 22:28:20 +0900

    :green_heart: Migrate from cypress to playwright

--- frontend/e2e/textusm.spec.ts
+++ frontend/e2e/textusm.spec.ts
@@ -28,8 +28,36 @@ test('Change background color of user story map', async ({ page }) => {
   await page.locator('[data-test-id="new-usm"]').first().click();
   await page.locator('[data-test-id="card-6"]').first().click();
   await page.locator('[data-test-id="background-color-context-menu"]').click();
-  await page.locator('.lime').click();
+  await page.locator('[data-test-id="color-lime"]').click();
   await page.waitForTimeout(500);
 
   await expect(await page.locator('[data-test-id="card-6"] > .ts-card').getAttribute('fill')).toBe(`#00ff00`);
+});
+
+test('Change foreground color of user story map', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  await page.locator('[data-test-id="new-menu"]').first().click();
+  await page.locator('[data-test-id="new-usm"]').first().click();
+  await page.locator('[data-test-id="card-6"]').first().click();
+  await page.locator('[data-test-id="foreground-color-context-menu"]').click();
+  await page.locator('[data-test-id="color-lime"]').click();
+  await page.waitForTimeout(500);
+
+  await expect(await page.locator('[data-test-id="card-6"] > text').getAttribute('color')).toBe(`#00ff00`);
+});
+
+test('Change background color settings', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  await page.locator("a[data-test-id='header-settings']").first().click();
+  await page.locator("div[data-test-id='background-color']").first().click();
+  await page.locator("div[data-test-id='background-color'] > div:nth-of-type(2) > div:first-child").first().click();
+  await page.locator("div[data-test-id='header-back']").first().click();
+
+  const backgroundColor = await page.locator('#usm').evaluate((el) => {
+    return window.getComputedStyle(el).getPropertyValue('background-color');
+  });
+
+  await expect(backgroundColor).toBe(`rgb(254, 254, 254)`);
 });
