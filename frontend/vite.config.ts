013bfa285e2604339b9d47b67c4b8b9ca7822c9f
harehare <harehare1110@gmail.com>
2024-09-02 22:26:11 +0900

    :rewind: Fix vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'node:path';
 import elmPlugin from 'vite-plugin-elm';
 import { sentryVitePlugin } from '@sentry/vite-plugin';
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 import { createHtmlPlugin } from 'vite-plugin-html';
 
@@ -37,27 +37,27 @@ export default defineConfig(({ mode }) => ({
       },
     },
     chunkSizeWarningLimit: 1024,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('jspdf') || id.includes('html2canvas')) {
-            return 'vendor-pdf';
-          }
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks(id) {
+    //       if (id.includes('jspdf') || id.includes('html2canvas')) {
+    //         return 'vendor-pdf';
+    //       }
 
-          if (id.includes('svgo')) {
-            return 'vendor-svgo';
-          }
+    //       if (id.includes('svgo')) {
+    //         return 'vendor-svgo';
+    //       }
 
-          if (id.includes('monaco-editor')) {
-            return 'vendor-monaco';
-          }
+    //       if (id.includes('monaco-editor')) {
+    //         return 'vendor-monaco';
+    //       }
 
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-      },
-    },
+    //       if (id.includes('node_modules')) {
+    //         return 'vendor';
+    //       }
+    //     },
+    //   },
+    // },
   },
   define: Object.fromEntries(env.map((key) => [`process.env.${key}`, JSON.stringify(process.env[key])])),
   plugins: [
@@ -88,6 +88,7 @@ export default defineConfig(({ mode }) => ({
         ],
       },
     }),
+    splitVendorChunkPlugin(),
     ...(mode === 'production'
       ? [
           // eslint-disable-next-line new-cap
