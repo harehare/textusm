e87eb8210217332b97e5741f40bf2922f2e89139
harehare <harehare1110@gmail.com>
2024-10-31 23:53:38 +0900

    :sparkles: Add init.lua

---
+++ lua/textusm/init.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+---@class Config
+local config = {
+}
+
+
+M.setup = function(config)
+  -- TODO:
+end
+
+M.render = function()
+  -- TODO:
+  local buf = bufnr or vim.api.nvim_get_current_buf()
+end
+
+return M
